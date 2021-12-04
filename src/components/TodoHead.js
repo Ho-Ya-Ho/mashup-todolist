e37d6ca73ebc122b10350d1c1881a8062df52f69
Ho-Ya-Ho <rlarkdgh1056@naver.com>
2021-12-05 03:12:26 +0900

    mashup-todolist

--- src/components/TodoHead.js
+++ src/components/TodoHead.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useTodoState } from '../TodoContext';
 
 const TodoHeadBlock = styled.div`
   padding-top: 48px;
@@ -25,12 +26,24 @@ const TodoHeadBlock = styled.div`
   }
 `;
 
+const today = new Date();
+const dateString = today.toLocaleDateString('ko-KR', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+});
+const dayName = today.toLocaleDateString('ko-KR', { weekday: 'long' });
+
+
 function TodoHead() {
+    const todos = useTodoState();
+    const undoneTasks = todos.filter(todo => !todo.done); //할 일 개수 표시(TodoContext.js에서 done을 기준으로 판별)
+
     return (
         <TodoHeadBlock>
-            <h1>2019년 7월 10일</h1>
-            <div className="day">수요일</div>
-            <div className="tasks-left">할 일 2개 남음</div>
+            <h1>{dateString}</h1>
+            <div className="day">{dayName}</div>
+            <div className="tasks-left">할 일 {undoneTasks.length}개 남음</div>
         </TodoHeadBlock>
     );
 }
